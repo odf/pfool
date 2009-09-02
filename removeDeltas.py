c15087adf6d59854c2d5c0ffff8a92f4c4a4a806
odf <olaf.delgado@gmail.com>
2009-09-02 22:52:49 +1000

    Made scripts executable; added usage messages to some ; added .hgignore file.

--- removeDeltas.py
+++ removeDeltas.py
@@ -3,6 +3,11 @@
 import sys
 import poserFile
 
+if len(sys.argv) < 2:
+    print "Usage: %s infile outfile" % sys.argv[0]
+    print "  Strips all deltas from a Poser file leaving morph channels empty."
+    sys.exit(1)
+
 content = poserFile.PoserFile(file(sys.argv[1]))
 content.root.delete('actor', 'channels', 'targetGeom',
                     'indexes|numbDeltas|deltas')
--- removeMorphs.py
+++ removeMorphs.py
@@ -3,6 +3,11 @@
 import sys
 import poserFile
 
+if len(sys.argv) < 2:
+    print "Usage: %s infile outfile" % sys.argv[0]
+    print "  Completely removes all morph channels from a Poser file."
+    sys.exit(1)
+
 content = poserFile.PoserFile(file(sys.argv[1]))
 content.root.delete('actor', 'channels', 'targetGeom')
 content.writeTo(file(sys.argv[2], 'w'))
