fbe3d3f09fadf91e2bde1042af8c7ca55f04cb3f
odf <olaf.delgado@gmail.com>
2009-09-07 00:01:19 +1000

    renameChannels.py: added a usage message.

--- renameChannels.py
+++ renameChannels.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import re, sys
+import os.path, re, sys
 from poserFile import *
 
 
@@ -18,6 +18,36 @@ class EchoDict(dict):
             return key
 
 if __name__ == "__main__":
+    if len(sys.argv) < 5:
+        print re.sub("<prog>", os.path.basename(sys.argv[0]), """\
+  Usage: [python] <prog> infile outfile pattern replacement
+
+    Changes internal channel names for targetGeom and valueParm
+    channels and updates references to renamed channels.  Regular
+    expression syntax is used for pattern and replacement.
+
+    Requires a properly installed Python 2.5 or newer (see
+    python.org).
+
+  Examples:
+
+    <prog> a.cr2 b.cr2 CTRL Ctrl-
+
+	Reads the file a.cr2, changes "CTRL" anywhere in a channel
+	name to "Ctrl-" and writes the result to the file b.cr2.
+
+    <prog> a.cr2 b.cr2 "^CTRL" Ctrl-
+
+	The same, but only replaces "CTRL" if it appears at the
+	beginning of the name.
+
+    <prog> a.cr2 b.cr2 "([a-z])([A-Z])" "\1_\2"
+
+	Inserts an underscore between words in channels names.
+""")
+
+        sys.exit(1)
+
     content = PoserFile(file(sys.argv[1]))
     pattern = re.compile(sys.argv[3])
     replacement = sys.argv[4]
