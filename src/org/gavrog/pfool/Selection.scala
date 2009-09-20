1220243ce2247323359a206222f70760b790c69f
odf <olaf.delgado@gmail.com>
2009-09-20 19:11:03 +1000

    Better implementation of the Selection class.

--- src/org/gavrog/pfool/Selection.scala
+++ src/org/gavrog/pfool/Selection.scala
@@ -1,25 +1,44 @@
 package org.gavrog.pfool
 
 object Selection {
-    implicit def asSelection(node: Line) = new Selection(node)
-    implicit def asSelection(doc: Document) = new Selection(doc.root)
+    implicit def asSelection(node: Line) = Selection(node)
+    implicit def asSelection(doc: Document) = Selection(doc.root)
+    
+    def apply(nodes: Line*) = new Selection(new Selection(null) {
+        override def elements = nodes.elements
+    })
 }
 
-class Selection(nodes: Line*) extends Iterable[Line] {
-    def elements: Iterator[Line] = nodes.elements
+class Selection(base: Selection,
+                axis: Line => Iterable[Line],
+                filter: Line => Boolean)
+    extends Iterable[Line]
+{
+    def this(base: Selection, axis: Line => Iterable[Line]) =
+        this(base, axis, n => true)
+  
+    def this(base: Selection) = this(base, List(_))
+  
+    def elements =
+        for (n <- base.elements; c <- axis(n).elements if filter(c)) yield c
+  
+    def \(p: String) = new Selection(this, _.children, _.matches(p))
     
-    private def extend(f: Line => Iterator[Line]) = {
-        val nodes = this.elements
-        new Selection {
-            override def elements = for (n <- nodes; c <- f(n)) yield c
-        }
-    }
+    def \\(p: String) = new Selection(this, _.descendants, _.matches(p))
     
-    def \(p: String) = extend(_.children.elements.filter(_.matches(p)))
+    def \!(p: String) = new Selection(this, _.children, !_.matches(p))
     
-    def \\(p: String) = extend(_.descendants.elements.filter(_.matches(p)))
+    def \\!(p: String) = new Selection(this, _.descendants, !_.matches(p))
+
+    def &(p: String) = new Selection(base, axis, n => filter(n) && n.matches(p))
+
+    def |(p: String) = new Selection(base, axis, n => filter(n) || n.matches(p))
+
+    def &!(p: String) =
+        new Selection(base, axis, n => filter(n) && !n.matches(p))
+
+    def |!(p: String) =
+        new Selection(base, axis, n => filter(n) || !n.matches(p))
     
-    def \!(p: String) = extend(_.children.elements.filter(!_.matches(p)))
-    
-    def \\!(p: String) = extend(_.descendants.elements.filter(!_.matches(p)))
+    def ! = new Selection(base, axis, n => !filter(n))
 }
