7fb7e1e87e6a50fa55c00103c2455a0b92059d8c
odf <olaf.delgado@gmail.com>
2009-09-21 23:05:28 +1000

    Refactored Selection class (again).

--- src/org/gavrog/pfool/Selection.scala
+++ src/org/gavrog/pfool/Selection.scala
@@ -4,41 +4,51 @@ object Selection {
     implicit def asSelection(node: Line) = Selection(node)
     implicit def asSelection(doc: Document) = Selection(doc.root)
     
-    def apply(nodes: Line*) = new Selection(new Selection(null) {
+    def apply(nodes: Line*) = new Selection(new Selection[Line](null) {
         override def elements = nodes.elements
     })
+    
+    implicit def toMatcher(p: String) = new Matcher((n: Line) => n.matches(p))
 }
 
-class Selection(base: Selection,
-                axis: Line => Iterable[Line],
-                filter: Line => Boolean)
-    extends Iterable[Line]
+
+class Matcher[T](f: T => Boolean) extends (T => Boolean) {
+    def apply(arg: T) = f(arg)
+    
+    def unary_! = new Matcher[T](!this(_))
+    
+    def & (that: Matcher[T]) = new Matcher[T](n => this(n) && that(n))
+    
+    def | (that: Matcher[T]) = new Matcher[T](n => this(n) || that(n))
+    
+    def ^ (that: Matcher[T]) = new Matcher[T](n => this(n) ^ that(n))
+}
+
+
+class Selection[T <: { def children: Iterable[T] }](
+                base: Selection[T],
+                axis: T => Iterable[T],
+                test: T => Boolean)
+    extends Iterable[T]
 {
-    def this(base: Selection, axis: Line => Iterable[Line]) =
-        this(base, axis, n => true)
+    def this(base: Selection[T], axis: T => Iterable[T]) =
+        this(base, axis, _ => true)
+    
+    def this(base: Selection[T]) = this(base, List(_))
   
-    def this(base: Selection) = this(base, List(_))
-  
-    def elements =
-        for (n <- base.elements; c <- axis(n).elements if filter(c)) yield c
-  
-    def \(p: String) = new Selection(this, _.children, _.matches(p))
-    
-    def \\(p: String) = new Selection(this, _.descendants, _.matches(p))
-    
-    def \!(p: String) = new Selection(this, _.children, !_.matches(p))
-    
-    def \\!(p: String) = new Selection(this, _.descendants, !_.matches(p))
+    def elements = base.elements.flatMap(axis(_).elements).filter(test)
 
-    def &(p: String) = new Selection(base, axis, n => filter(n) && n.matches(p))
-
-    def |(p: String) = new Selection(base, axis, n => filter(n) || n.matches(p))
-
-    def &!(p: String) =
-        new Selection(base, axis, n => filter(n) && !n.matches(p))
-
-    def |!(p: String) =
-        new Selection(base, axis, n => filter(n) || !n.matches(p))
+    def children = new Selection[T](this, _.children.toStream)
     
-    def ! = new Selection(base, axis, n => !filter(n))
+    def descendants = {
+        def desc(n: T): Stream[T] =
+            Stream.concat(n.children.map(c => Stream.cons(c, desc(c))))
+        new Selection[T](this, desc)
+    }
+    
+    def &(m: Matcher[T]) = new Selection[T](base, axis, n => test(n) && m(n))
+    
+    def \(m: Matcher[T]) = children & m
+    
+    def \\(m: Matcher[T]) = descendants & m
 }
