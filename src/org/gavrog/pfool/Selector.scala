6da7c9f3ba37db2e234c384e72cdfee02d55eece
odf <olaf.delgado@gmail.com>
2010-07-24 23:58:30 +1000

    Simplified some selector code; added \! and \\! operators.

--- src/org/gavrog/pfool/Selector.scala
+++ src/org/gavrog/pfool/Selector.scala
@@ -2,21 +2,20 @@ package org.gavrog.pfool
 
 
 class Selector[T <: { def children: Iterable[T] }]
-               (f: Iterable[T] => Iterable[T])
+               (generate: Iterable[T] => Iterable[T])
 {
-  def apply = f
+  def apply = generate
     
-  def apply(f: T => Iterable[T]): Selector[T] =
-    new Selector[T](roots => this(roots).flatMap(f))
+  def apply(f: T => Iterable[T]) = new Selector[T](generate(_).flatMap(f))
     
   def children = this(_.children)
 
   private def desc(n: T): Iterable[T] =
-    n.children.flatMap(c => (Stream(c) ++ desc(c)))
+    n.children.flatMap(c => Stream(c) ++ desc(c))
     
   def descendants = this(desc(_))
 
-  def &(s: Selector[T]) = new Selector[T](roots => s(this(roots)))
+  def &(s: Selector[T]) = new Selector[T](this.apply andThen s.apply)
   def |(s: Selector[T]) =
     new Selector[T](roots => this(roots).toSet ++ s(roots))
   
@@ -25,10 +24,13 @@ class Selector[T <: { def children: Iterable[T] }]
     
   def \@(s: Selector[T]) = this & Filter(n => !s(n.children).isEmpty)
   def \\@(s: Selector[T]) = this & Filter(n => !s(desc(n)).isEmpty)
+    
+  def \!(s: Selector[T]) = this & Filter(n => s(n.children).isEmpty)
+  def \\!(s: Selector[T]) = this & Filter(n => s(desc(n)).isEmpty)
 }
 
 case class Filter[T <: { def children: Iterable[T] }](f: T => Boolean)
-	extends Selector[T](roots => roots.filter(f))
+	extends Selector[T](_.filter(f))
 {
   def unary_! = new Filter[T](!f(_))
 
